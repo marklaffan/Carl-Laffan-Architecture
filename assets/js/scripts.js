acd538950980d393e2354f2152ac3543b05ae720
Mark Laffan <marklaffan4@gmail.com>
2018-01-13 14:30:51 +0000

    added some basic funtionality

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,15 @@
-var h1 = document.querySelector("h1");
+var project = document.getElementsByClassName("links") [0];
+var resume = document.getElementsByClassName("links") [1];
+var contact = document.getElementsByClassName("links") [2];
 
-h1.style.color = "red";
+project.addEventListener("click", function(){
+    document.getElementById("projects").classList.toggle("projectList");
+});
+
+resume.addEventListener("click", function(){
+    document.getElementById("projects").classList.add("projectList");
+});
+
+contact.addEventListener("click", function(){
+    document.getElementById("projects").classList.add("projectList");
+});
